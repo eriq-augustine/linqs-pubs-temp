ae9238e43841ce689f644c5733b9a1316bedc5c6
Eriq Augustine <eriq.augustine@gmail.com>
2020-06-25 11:36:23 -0700

    Make key order consistent.

--- parse.py
+++ parse.py
@@ -14,6 +14,45 @@ REQUIRED_KEYS = [
     'venue',
 ]
 
+SORTED_KEYS = [
+    'type',
+    'authors',
+    'title',
+    'venue',
+    'year',
+
+    'publisher',
+    'address',
+    'keywords',
+    'pages',
+    'month',
+    'volume',
+    'chapter',
+    'editor',
+    'organization',
+    'note',
+    'series',
+    'number',
+    'doi',
+    'edition',
+
+    'links',
+
+    'abstract',
+]
+
+# In Python3, dicts remember the order that keys were inserted (and will iterate in that order).
+# The json library will use that ordering when outputting.
+# So, we will iterate through the keys in our order and put them into a new dict in the correct order.
+def sortKeys(data):
+    newData = {}
+
+    keys = sorted(list(data.keys()), key = lambda key: SORTED_KEYS.index(key))
+    for key in keys:
+        newData[key] = data[key]
+
+    return newData
+
 def main():
     for dirent in sorted(os.listdir(DATA_PATH)):
         path = os.path.join(DATA_PATH, dirent)
@@ -22,7 +61,7 @@ def main():
         # print(path)
 
         with open(path, 'r') as file:
-            data = json.load(file)
+            data = sortKeys(json.load(file))
 
         for requiredKey in REQUIRED_KEYS:
             if (requiredKey not in data):
@@ -34,6 +73,10 @@ def main():
         if (not dirent.startswith(lastName.lower())):
             # print("mv -i '%s' '%s'" % (path, re.sub(r'/\w+-', "/%s-" % (lastName.lower()), path)))
             raise ValueError("Filename should start with author last name (%s): [%s]." % (lastName.lower(), path))
+
+        # TEST
+        with open(path, 'w') as file:
+            json.dump(data, file, indent = 4)
 
 if (__name__ == '__main__'):
     main()
