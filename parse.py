9145d964c0a385f9efaadacefc360a3b8ebf9185
Eriq Augustine <eriq.augustine@gmail.com>
2020-06-25 11:24:01 -0700

    Renamed files with last names.

--- parse.py
+++ parse.py
@@ -2,23 +2,38 @@
 
 import json
 import os
+import re
 
 DATA_PATH = 'data'
 
+REQUIRED_KEYS = [
+    'type',
+    'authors',
+    'title',
+    'year',
+    'venue',
+]
+
 def main():
-    for dirent in os.listdir(DATA_PATH):
+    for dirent in sorted(os.listdir(DATA_PATH)):
         path = os.path.join(DATA_PATH, dirent)
 
         # TEST
-        print(path)
+        # print(path)
 
         with open(path, 'r') as file:
             data = json.load(file)
 
-        # TEST
-        # print(path)
-        # print(data)
-        # break
+        for requiredKey in REQUIRED_KEYS:
+            if (requiredKey not in data):
+                raise ValueError("Could not find requried key (%s) in %s." % (requiredKey, path))
+
+        firstAuthor = data['authors'][0]
+        lastName = firstAuthor.split(' ')[-1]
+
+        if (not dirent.startswith(lastName.lower())):
+            # print("mv -i '%s' '%s'" % (path, re.sub(r'/\w+-', "/%s-" % (lastName.lower()), path)))
+            raise ValueError("Filename should start with author last name (%s): [%s]." % (lastName.lower(), path))
 
 if (__name__ == '__main__'):
     main()
